69f8f9d30ad7c0bd49e7a9e91606a0ad3475a44b
Salsabil007 <salsabilarabishusmi007@gmail.com>
2017-11-01 22:04:22 +0600

    model and admin site

--- Django_proj/mysite/polls/views.py
+++ Django_proj/mysite/polls/views.py
@@ -1,11 +1,5 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, Shusmi. You're at the polls index.")
