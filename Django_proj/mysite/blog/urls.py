3cba55989598f60da40af33362326427ed0184ef
Salsabil007 <salsabilarabishusmi007@gmail.com>
2017-12-30 11:11:20 +0600

    blog edit done

--- Django_proj/mysite/blog/urls.py
+++ Django_proj/mysite/blog/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^index2/$', views.index2, name='index2'),
     url(r'^addblog/$', views.add_bloginfo,name='add_bloginfo' ),
     url(r'^(?P<aaa_id>\d+)/addcontent/$', views.add_content,name='add_content' ),
+    url(r'^(?P<table_id>\d+)/(?P<part_id>\d+)/getcontent/$',views.get_content,name='get_content' ),
+    url(r'^(?P<table_id>\d+)/(?P<part_id>\d+)/updateblog/$',views.update_blog,name='update_blog' ),
 ]
--- Django_proj/mysite/blog/views.py
+++ Django_proj/mysite/blog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect, render
 from blog.forms import BlogForm, ContentForm
 from django.http import HttpResponse
 
-from .models import Blog_Table
+from .models import Blog_Table,Blog_Part
 from django.template import loader
 from django.shortcuts import get_object_or_404
 
@@ -16,6 +16,17 @@ def index2(request):
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
+
+
+def get_content(request, table_id, part_id):
+    #content=Blog_Part.objects.filter(id=part_id,blogID=table_id)
+    #template = loader.get_template('blog/contentofblog.html')
+    #context = {
+    #    'content': content,
+    #}
+    #return HttpResponse(template.render(context, request))
+    content = get_object_or_404(Blog_Part, pk=part_id,blogID=table_id)
+    return render(request, 'blog/contentofblog.html', {'content': content})
 
 
 def index(request):
@@ -42,7 +53,7 @@ def add_content(request, aaa_id):
         if form.is_valid():
             #form.userID=a.id
             blog_item = form.save(commit=False)
-            blog_item.userID = a
+            blog_item.blogID = a
 
             blog_item.save()
 
@@ -51,3 +62,17 @@ def add_content(request, aaa_id):
     else:
         form = ContentForm()
     return render(request, 'blog/content.html', {'form': form})
+
+def update_blog(request, table_id, part_id):
+    instance=Blog_Part.objects.get(pk=part_id,blogID=table_id)
+    if request.method == "POST":
+        form= ContentForm(request.POST, instance=instance)
+        if form.is_valid():
+            editedentry= form.save()
+            editedentry.save()
+            return redirect('/blog/')
+
+    else:
+        form= ContentForm(instance=instance)
+
+        return render(request, 'blog/content.html', {'form': form})
